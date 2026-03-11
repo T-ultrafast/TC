604ee3de8446a129e7023d07f61a75f7daa1e928
mo_mo <ayodeletemmy@gmail.com>
2026-03-11 17:05:21 -0400

    Header Fix

--- tclens-web/src/app/page.tsx
+++ tclens-web/src/app/page.tsx
@@ -1,2 +1,13 @@
-// Re-export the marketing home page as the root page
-export { default } from "./(marketing)/page";
+import { Navbar } from "@/components/Navbar";
+import MarketingPage from "./(marketing)/page";
+
+export default function Home() {
+    return (
+        <div className="min-h-screen flex flex-col">
+            <Navbar />
+            <main className="flex-1">
+                <MarketingPage />
+            </main>
+        </div>
+    );
+}
